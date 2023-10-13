4977aee2595b821fef881698d2a1af70f017b6ff
jas26 <chilaka.jaswanth@gmail.com>
2023-10-13 12:34:24 -0700

    check for commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test application to build auth layer in React application
         </p>
         <a
           className="App-link"
